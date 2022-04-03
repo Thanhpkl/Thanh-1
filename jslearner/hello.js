0dd841d6d81b7855b02c0aa5536fab70e7a456cf
Thanhpkl <dotienthanhthanh6@gmail.com>
2022-04-03 19:11:07 +0700

    hihi

---
+++ jslearner/hello.js
@@ -0,0 +1,29 @@
+// function phuongtrinhphuctap(a,b,c,d){
+// //     function phuctap0(x){
+       
+// //        function phuctap1(y){
+// //            let z1 = y * y;
+// //            return z1
+// //        }
+// //        return phuctap1(x) + phuctap1(x);
+       
+// //     }
+// //     return phuctap0(a) + phuctap0(b) + phuctap0(c) + phuctap0(d) 
+// // }
+// // console.log(phuongtrinhphuctap(1,2,3,4));
+// // console.log(5*5 + 6*6 +5*5 +4*4)
+
+// // string = "hello cac ban";
+
+// // console.log(string.slice(-12,-5));
+// // function swith(){
+// //     var hello = document.body
+// //     hello.classList.toggle('blackmode')
+// var text = "hello where is the last hello"
+// alert(text.startsWith("hello",0t))
+
+
+let ten = "thanh";
+console.log("xin chao moi nguoi minh ten" + ten);
+console.log("xin chao moi nguoi minh ten" + ten + "  ");
+console.log(`Xin chao moi nguoi minh ten${ten}`);
